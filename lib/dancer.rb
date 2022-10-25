9736900982071349eb7c6727ad14f0f84d0db288
moringa <oscarokumu47@gmail.com>
2022-10-25 14:22:35 +0300

    Done

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,4 +4,10 @@ require_relative './fancy_dance'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
 end
--- lib/kid.rb
+++ lib/kid.rb
@@ -4,4 +4,10 @@ require_relative './meta_dancing_module'
 class Kid
     extend FancyDance::ClassMethods
     include FancyDance::InstanceMethods
+
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
   end
