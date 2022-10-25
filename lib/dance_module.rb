7d33d34dd4ede500c7a0890412fa8733bdcca008
moringa <oscarokumu47@gmail.com>
2022-10-25 14:20:04 +0300

    Done

---
+++ lib/dance_module.rb
@@ -0,0 +1,14 @@
+module Dance
+    def twirl
+        "Look how high I'm jumping"
+    end
+    def jumping
+        "I am jumping"
+    end
+    def pirouette
+        "I am a pirouette"
+    end
+    def take_a_bow
+        "Thank you. It was a pleasure to dance for you all"
+    end
+end
---
+++ lib/meta_dancing_module.rb
@@ -0,0 +1,5 @@
+module meta_dancing_module
+    def metadata
+        "This class producess objects that love to dance"
+    end
+end
